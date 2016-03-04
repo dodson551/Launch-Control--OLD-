b1decaa4319b4d13bd114af91834398a8370060d
Colin Christopher <colin.christo4@gmail.com>
2016-03-03 19:39:34 -0800

    Added Colin's frmFCC to project

--- Launch_Control/frmFCC.Designer.vb
+++ Launch_Control/frmFCC.Designer.vb
@@ -20,64 +20,321 @@ Partial Class frmFCC
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmFCC))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tb_IPAddress = New System.Windows.Forms.TextBox()
+        Me.gb_NetworkConnections = New System.Windows.Forms.GroupBox()
+        Me.btn_Test = New System.Windows.Forms.Button()
+        Me.btn_Disconnect = New System.Windows.Forms.Button()
+        Me.btn_Connect = New System.Windows.Forms.Button()
+        Me.tb_Port = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.btn_Center = New System.Windows.Forms.Button()
+        Me.btn_Abort = New System.Windows.Forms.Button()
+        Me.btn_X = New System.Windows.Forms.Button()
+        Me.btn_Circle = New System.Windows.Forms.Button()
+        Me.btn_PID = New System.Windows.Forms.Button()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.lb_ConnectionStatus = New System.Windows.Forms.ListBox()
+        Me.GroupBox5 = New System.Windows.Forms.GroupBox()
+        Me.dgv_FCC = New System.Windows.Forms.DataGridView()
+        Me.btn_OpenValve1 = New System.Windows.Forms.Button()
+        Me.btn_OpenValve2 = New System.Windows.Forms.Button()
+        Me.btn_CloseValve2 = New System.Windows.Forms.Button()
+        Me.btn_CloseValve1 = New System.Windows.Forms.Button()
+        Me.gb_NetworkConnections.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.GroupBox5.SuspendLayout()
+        CType(Me.dgv_FCC, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'TextBox1
+        'Label1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(8, 8)
-        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
-        Me.TextBox1.Multiline = True
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(129, 49)
-        Me.TextBox1.TabIndex = 0
-        Me.TextBox1.Text = "This is the form for the flight control computer."
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.Label1.Location = New System.Drawing.Point(18, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(125, 26)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "IP Address:"
         '
-        'TextBox2
+        'tb_IPAddress
         '
-        Me.TextBox2.Location = New System.Drawing.Point(8, 61)
-        Me.TextBox2.Margin = New System.Windows.Forms.Padding(2)
-        Me.TextBox2.Multiline = True
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(129, 49)
-        Me.TextBox2.TabIndex = 1
-        Me.TextBox2.Text = "Make stuff on this page pleaseeee."
+        Me.tb_IPAddress.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.tb_IPAddress.Location = New System.Drawing.Point(149, 17)
+        Me.tb_IPAddress.Name = "tb_IPAddress"
+        Me.tb_IPAddress.Size = New System.Drawing.Size(257, 32)
+        Me.tb_IPAddress.TabIndex = 1
         '
-        'TextBox3
+        'gb_NetworkConnections
         '
-        Me.TextBox3.Location = New System.Drawing.Point(8, 114)
-        Me.TextBox3.Margin = New System.Windows.Forms.Padding(2)
-        Me.TextBox3.Multiline = True
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(129, 49)
-        Me.TextBox3.TabIndex = 2
-        Me.TextBox3.Text = "hopefully changing the gitignore file helped."
+        Me.gb_NetworkConnections.Controls.Add(Me.btn_Test)
+        Me.gb_NetworkConnections.Controls.Add(Me.btn_Disconnect)
+        Me.gb_NetworkConnections.Controls.Add(Me.btn_Connect)
+        Me.gb_NetworkConnections.Controls.Add(Me.tb_Port)
+        Me.gb_NetworkConnections.Controls.Add(Me.Label2)
+        Me.gb_NetworkConnections.Controls.Add(Me.Label1)
+        Me.gb_NetworkConnections.Controls.Add(Me.tb_IPAddress)
+        Me.gb_NetworkConnections.Location = New System.Drawing.Point(12, 12)
+        Me.gb_NetworkConnections.Name = "gb_NetworkConnections"
+        Me.gb_NetworkConnections.Size = New System.Drawing.Size(632, 106)
+        Me.gb_NetworkConnections.TabIndex = 2
+        Me.gb_NetworkConnections.TabStop = False
+        Me.gb_NetworkConnections.Text = "Network Connections"
+        '
+        'btn_Test
+        '
+        Me.btn_Test.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.btn_Test.Location = New System.Drawing.Point(484, 55)
+        Me.btn_Test.Name = "btn_Test"
+        Me.btn_Test.Size = New System.Drawing.Size(95, 36)
+        Me.btn_Test.TabIndex = 6
+        Me.btn_Test.Text = "Test"
+        Me.btn_Test.UseVisualStyleBackColor = True
+        '
+        'btn_Disconnect
+        '
+        Me.btn_Disconnect.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.btn_Disconnect.Location = New System.Drawing.Point(275, 55)
+        Me.btn_Disconnect.Name = "btn_Disconnect"
+        Me.btn_Disconnect.Size = New System.Drawing.Size(131, 36)
+        Me.btn_Disconnect.TabIndex = 5
+        Me.btn_Disconnect.Text = "Disconnect"
+        Me.btn_Disconnect.UseVisualStyleBackColor = True
+        '
+        'btn_Connect
+        '
+        Me.btn_Connect.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.btn_Connect.Location = New System.Drawing.Point(149, 55)
+        Me.btn_Connect.Name = "btn_Connect"
+        Me.btn_Connect.Size = New System.Drawing.Size(120, 36)
+        Me.btn_Connect.TabIndex = 4
+        Me.btn_Connect.Text = "Connect"
+        Me.btn_Connect.UseVisualStyleBackColor = True
+        '
+        'tb_Port
+        '
+        Me.tb_Port.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.tb_Port.Location = New System.Drawing.Point(484, 17)
+        Me.tb_Port.Name = "tb_Port"
+        Me.tb_Port.Size = New System.Drawing.Size(126, 32)
+        Me.tb_Port.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.Label2.Location = New System.Drawing.Point(420, 20)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 26)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Port:"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btn_CloseValve2)
+        Me.GroupBox1.Controls.Add(Me.btn_CloseValve1)
+        Me.GroupBox1.Controls.Add(Me.btn_OpenValve2)
+        Me.GroupBox1.Controls.Add(Me.btn_OpenValve1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.GroupBox1.Location = New System.Drawing.Point(339, 234)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(305, 308)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "RCS"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.btn_Center)
+        Me.GroupBox3.Controls.Add(Me.btn_Abort)
+        Me.GroupBox3.Controls.Add(Me.btn_X)
+        Me.GroupBox3.Controls.Add(Me.btn_Circle)
+        Me.GroupBox3.Controls.Add(Me.btn_PID)
+        Me.GroupBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.GroupBox3.Location = New System.Drawing.Point(12, 234)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(321, 308)
+        Me.GroupBox3.TabIndex = 5
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Test Sequences"
+        '
+        'btn_Center
+        '
+        Me.btn_Center.Location = New System.Drawing.Point(166, 124)
+        Me.btn_Center.Name = "btn_Center"
+        Me.btn_Center.Size = New System.Drawing.Size(126, 63)
+        Me.btn_Center.TabIndex = 3
+        Me.btn_Center.Text = "Center"
+        Me.btn_Center.UseVisualStyleBackColor = True
+        '
+        'btn_Abort
+        '
+        Me.btn_Abort.Location = New System.Drawing.Point(22, 214)
+        Me.btn_Abort.Name = "btn_Abort"
+        Me.btn_Abort.Size = New System.Drawing.Size(270, 63)
+        Me.btn_Abort.TabIndex = 4
+        Me.btn_Abort.Text = "ABORT"
+        Me.btn_Abort.UseVisualStyleBackColor = True
+        '
+        'btn_X
+        '
+        Me.btn_X.Location = New System.Drawing.Point(22, 34)
+        Me.btn_X.Name = "btn_X"
+        Me.btn_X.Size = New System.Drawing.Size(126, 63)
+        Me.btn_X.TabIndex = 0
+        Me.btn_X.Text = "X"
+        Me.btn_X.UseVisualStyleBackColor = True
+        '
+        'btn_Circle
+        '
+        Me.btn_Circle.Image = CType(resources.GetObject("btn_Circle.Image"), System.Drawing.Image)
+        Me.btn_Circle.Location = New System.Drawing.Point(166, 34)
+        Me.btn_Circle.Name = "btn_Circle"
+        Me.btn_Circle.Size = New System.Drawing.Size(126, 63)
+        Me.btn_Circle.TabIndex = 1
+        Me.btn_Circle.UseVisualStyleBackColor = True
+        '
+        'btn_PID
+        '
+        Me.btn_PID.Location = New System.Drawing.Point(22, 124)
+        Me.btn_PID.Name = "btn_PID"
+        Me.btn_PID.Size = New System.Drawing.Size(126, 63)
+        Me.btn_PID.TabIndex = 2
+        Me.btn_PID.Text = "PID"
+        Me.btn_PID.UseVisualStyleBackColor = True
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.lb_ConnectionStatus)
+        Me.GroupBox4.Location = New System.Drawing.Point(12, 124)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(632, 104)
+        Me.GroupBox4.TabIndex = 5
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Connection Status"
+        '
+        'lb_ConnectionStatus
+        '
+        Me.lb_ConnectionStatus.BackColor = System.Drawing.Color.Black
+        Me.lb_ConnectionStatus.ForeColor = System.Drawing.Color.Lime
+        Me.lb_ConnectionStatus.FormattingEnabled = True
+        Me.lb_ConnectionStatus.Location = New System.Drawing.Point(6, 15)
+        Me.lb_ConnectionStatus.Name = "lb_ConnectionStatus"
+        Me.lb_ConnectionStatus.Size = New System.Drawing.Size(620, 82)
+        Me.lb_ConnectionStatus.TabIndex = 0
+        '
+        'GroupBox5
+        '
+        Me.GroupBox5.Controls.Add(Me.dgv_FCC)
+        Me.GroupBox5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.GroupBox5.Location = New System.Drawing.Point(12, 548)
+        Me.GroupBox5.Name = "GroupBox5"
+        Me.GroupBox5.Size = New System.Drawing.Size(632, 308)
+        Me.GroupBox5.TabIndex = 7
+        Me.GroupBox5.TabStop = False
+        Me.GroupBox5.Text = "Test Sequence Status"
+        '
+        'dgv_FCC
+        '
+        Me.dgv_FCC.BackgroundColor = System.Drawing.Color.DarkGray
+        Me.dgv_FCC.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv_FCC.Location = New System.Drawing.Point(6, 31)
+        Me.dgv_FCC.Name = "dgv_FCC"
+        Me.dgv_FCC.Size = New System.Drawing.Size(620, 271)
+        Me.dgv_FCC.TabIndex = 6
+        '
+        'btn_OpenValve1
+        '
+        Me.btn_OpenValve1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.btn_OpenValve1.Location = New System.Drawing.Point(6, 34)
+        Me.btn_OpenValve1.Name = "btn_OpenValve1"
+        Me.btn_OpenValve1.Size = New System.Drawing.Size(145, 63)
+        Me.btn_OpenValve1.TabIndex = 0
+        Me.btn_OpenValve1.Text = "Open Valve 1"
+        Me.btn_OpenValve1.UseVisualStyleBackColor = True
+        '
+        'btn_OpenValve2
+        '
+        Me.btn_OpenValve2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.btn_OpenValve2.Location = New System.Drawing.Point(154, 34)
+        Me.btn_OpenValve2.Name = "btn_OpenValve2"
+        Me.btn_OpenValve2.Size = New System.Drawing.Size(145, 63)
+        Me.btn_OpenValve2.TabIndex = 8
+        Me.btn_OpenValve2.Text = "Open Valve 2"
+        Me.btn_OpenValve2.UseVisualStyleBackColor = True
+        '
+        'btn_CloseValve2
+        '
+        Me.btn_CloseValve2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.btn_CloseValve2.Location = New System.Drawing.Point(154, 124)
+        Me.btn_CloseValve2.Name = "btn_CloseValve2"
+        Me.btn_CloseValve2.Size = New System.Drawing.Size(145, 63)
+        Me.btn_CloseValve2.TabIndex = 10
+        Me.btn_CloseValve2.Text = "Close Valve 2"
+        Me.btn_CloseValve2.UseVisualStyleBackColor = True
+        '
+        'btn_CloseValve1
+        '
+        Me.btn_CloseValve1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.btn_CloseValve1.Location = New System.Drawing.Point(6, 124)
+        Me.btn_CloseValve1.Name = "btn_CloseValve1"
+        Me.btn_CloseValve1.Size = New System.Drawing.Size(145, 63)
+        Me.btn_CloseValve1.TabIndex = 9
+        Me.btn_CloseValve1.Text = "Close Valve 1"
+        Me.btn_CloseValve1.UseVisualStyleBackColor = True
         '
         'frmFCC
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(410, 400)
-        Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.ClientSize = New System.Drawing.Size(1584, 861)
+        Me.Controls.Add(Me.GroupBox5)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.gb_NetworkConnections)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "frmFCC"
         Me.Text = "frmFCC"
+        Me.gb_NetworkConnections.ResumeLayout(False)
+        Me.gb_NetworkConnections.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox5.ResumeLayout(False)
+        CType(Me.dgv_FCC, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox3 As TextBox
-
-    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
-
-    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents tb_IPAddress As TextBox
+    Friend WithEvents gb_NetworkConnections As GroupBox
+    Friend WithEvents btn_Disconnect As Button
+    Friend WithEvents btn_Connect As Button
+    Friend WithEvents tb_Port As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btn_Abort As Button
+    Friend WithEvents btn_Center As Button
+    Friend WithEvents btn_PID As Button
+    Friend WithEvents btn_Circle As Button
+    Friend WithEvents btn_X As Button
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents lb_ConnectionStatus As ListBox
+    Friend WithEvents btn_Test As Button
+    Friend WithEvents GroupBox5 As GroupBox
+    Friend WithEvents dgv_FCC As DataGridView
+    Friend WithEvents btn_CloseValve2 As Button
+    Friend WithEvents btn_CloseValve1 As Button
+    Friend WithEvents btn_OpenValve2 As Button
+    Friend WithEvents btn_OpenValve1 As Button
 End Class
--- Launch_Control/frmFCC.vb
+++ Launch_Control/frmFCC.vb
@@ -1,1 +1,240 @@
-﻿
+﻿'====================================================
+'
+' Things to do for flight control computer
+'
+'  Things to do next TIME::::
+'    + Complete the GUI layout
+'    + Find a way to loop the Listen() function so that we can receive live data readouts!
+'    + Also Fix original Send_Receive Function to make sure it works for regular tests
+'
+'
+'====================================================
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+
+Public Class frmFCC
+    Dim ipAddr As IPAddress
+    Dim port As Integer
+    Dim fccSocket As Socket
+    Dim dt As DataTable = New DataTable()
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles tb_IPAddress.TextChanged
+
+    End Sub
+
+    Private Sub frmFCC_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tb_IPAddress.Text = "192.168.2.2"
+        tb_Port.Text = "7777"
+        lb_ConnectionStatus.Items.Add("Waiting to connect to network...")
+        'lb_ConnectionStatus.Enabled = False ' makes Connection status box read only
+
+        dt.Columns.Add("Events")
+        dt.Columns.Add("Time Stamp")
+    End Sub
+
+    Private Sub btn_Connect_Click(sender As Object, e As EventArgs) Handles btn_Connect.Click
+        ' copied most of the connection code from Jake's main LCS connection functions
+        Try
+            ' creates socket to make connection
+            ipAddr = IPAddress.Parse(tb_IPAddress.Text)
+            port = tb_Port.Text
+            fccSocket = New Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+            Dim result = fccSocket.BeginConnect(ipAddr, port, Nothing, Nothing)
+            Dim success = result.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(3))
+
+            ' if the connection isnt successful, notify the user via ConnectionStatus Box
+            If Not success Then
+                lb_ConnectionStatus.Items.Add("Error Connecting to " & tb_IPAddress.Text.ToString())
+
+            Else
+                'if the connection is successful then stop the user from changing the connection info
+                tb_IPAddress.Enabled = False
+                tb_Port.Enabled = False
+                btn_Connect.Enabled = False
+
+                ' notify user of connection success
+                lb_ConnectionStatus.Items.Add("Connection Established. " & DateTime.Now.ToShortTimeString())
+
+
+
+            End If
+
+
+        Catch ex As Exception
+            lb_ConnectionStatus.Items.Add("Error making the connection... Please Try Again.")
+
+        End Try
+
+    End Sub
+
+    Private Sub btn_Test_Click(sender As Object, e As EventArgs) Handles btn_Test.Click
+
+        Dim msg = "hi"
+        Dim receiptStr = Send(msg)
+
+        If receiptStr = "hello" Then
+            lb_ConnectionStatus.Items.Add("Connection Test Successful!" & DateTime.Now.ToShortTimeString())
+        Else
+            lb_ConnectionStatus.Items.Add("Connection Test Failed... Did not receive a response" & DateTime.Now.ToShortTimeString())
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' This function will serve as the basic send and receive for the socket connection. 
+    ''' </summary>
+    ''' <param name="sendThis"></param>
+    ''' <returns> the message received from the server </returns>
+    Public Function Send(sendThis As String) 'As String
+        Try
+            Dim message As Byte() = Encoding.ASCII.GetBytes(sendThis) ' converts string to ASCII bytes
+            Dim returnedMsg As String = Nothing
+            dgv_FCC.DataSource = dt ' make sure all data is printing to DGV
+            fccSocket.Send(message) ' sends byte array over socket connection
+
+            Do
+                returnedMsg = Listen()
+                dt.Rows.Add(returnedMsg)
+            Loop Until returnedMsg = "Complete"
+
+
+            dt.Rows.Add("Message Sent: " & sendThis & " " & DateTime.Now.ToShortTimeString())
+
+            'Dim receipt(1024) As Byte
+            'Dim receivedMsgLength As Integer = fccSocket.Receive(receipt) ' gets size and bytes 
+
+            'Dim receiptStr As String = Encoding.ASCII.GetString(receipt, 0, receivedMsgLength) ' converts message to string
+
+            'dt.Rows.Add("Message Received: " & receiptStr & " " & DateTime.Now.ToShortTimeString())
+
+            Return Nothing
+
+        Catch ex As Exception
+            dt.Rows.Add("Error sending/receiving the message.  Try again!")
+            Return Nothing
+        End Try
+
+    End Function
+
+    Public Function Listen() As String
+        Try
+            Dim receipt(1024) As Byte
+            Dim receivedMsgLength As Integer = fccSocket.Receive(receipt) ' gets size and bytes of returned msg
+
+            Dim receiptStr As String = Encoding.ASCII.GetString(receipt, 0, receivedMsgLength) ' converts message to string
+
+            dt.Rows.Add(receiptStr)
+
+            Return receiptStr
+
+        Catch ex As Exception
+            dt.Rows.Add("Error Listening to Connection")
+            Return Nothing
+        End Try
+    End Function
+
+
+    Private Sub btn_Disconnect_Click(sender As Object, e As EventArgs) Handles btn_Disconnect.Click
+        Try
+            fccSocket.Shutdown(SocketShutdown.Both)
+            Dim dis_result = fccSocket.BeginDisconnect(True, Nothing, Nothing)
+            Dim dis_success = dis_result.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(3))
+            If Not dis_success Then
+                lb_ConnectionStatus.Items.Add("Client software was not able to disconnect successfully. Please try again." & DateTime.Now.ToShortTimeString())
+            Else
+                lb_ConnectionStatus.Items.Add("Connection Successfully Disconnected..." & DateTime.Now.ToShortTimeString())
+                btn_Connect.Enabled = True
+                tb_IPAddress.Enabled = True
+                tb_Port.Enabled = True
+
+            End If
+        Catch ex As Exception
+            lb_ConnectionStatus.Items.Add(ex.Message & DateTime.Now.ToShortTimeString())
+        End Try
+    End Sub
+
+    Private Sub GroupBox3_Enter(sender As Object, e As EventArgs) Handles GroupBox3.Enter
+
+    End Sub
+
+    Private Sub btn_X_Click(sender As Object, e As EventArgs) Handles btn_X.Click
+        Dim sendThisMsg As String = "max"
+        Dim returnedMsg As String = Send(sendThisMsg)
+
+        If returnedMsg = "Complete" Then
+            dt.Rows.Add(returnedMsg & DateTime.Now.ToShortTimeString())
+            dgv_FCC.DataSource = dt
+        Else
+            dt.Rows.Add("Error Receiving X Pattern return message.  Check X Pattern status on server.")
+        End If
+    End Sub
+
+    Private Sub btn_Circle_Click(sender As Object, e As EventArgs) Handles btn_Circle.Click
+        Dim sendThisMsg As String = "min"
+        Dim returnedMsg As String = Send(sendThisMsg)
+
+        If returnedMsg = "Complete" Then
+            dt.Rows.Add(returnedMsg & DateTime.Now.ToShortTimeString())
+            dgv_FCC.DataSource = dt
+        Else
+            dt.Rows.Add("Error Receiving Circle Pattern return message.  Check Circle Pattern status on server.")
+
+        End If
+    End Sub
+
+    Private Sub btn_PID_Click(sender As Object, e As EventArgs) Handles btn_PID.Click
+        Dim sendThisMsg As String = "PID"
+        Dim returnedMsg As String = Send(sendThisMsg)
+
+        If returnedMsg = "PID Program started successfully." Then
+            dt.Rows.Add(returnedMsg & DateTime.Now.ToShortTimeString())
+            dgv_FCC.DataSource = dt
+        Else
+            dt.Rows.Add("Error Receiving PID return message.  Check PID status on server.")
+        End If
+    End Sub
+
+    Private Sub btn_Center_Click(sender As Object, e As EventArgs) Handles btn_Center.Click
+        Dim sendThisMsg As String = "center"
+        Dim returnedMsg As String = Nothing
+        returnedMsg = Send(sendThisMsg)
+        'Do
+        '    returnedMsg = Listen()
+
+        '    'If returnedMsg = Nothing Then
+        '    '    Exit Do
+        '    'End If
+
+        '    dt.Rows.Add(returnedMsg)
+        'Loop Until returnedMsg = "Complete"
+
+        If returnedMsg = Nothing Then
+            dt.Rows.Add("Heard Nothing, Check Centering Status")
+        Else
+            dt.Rows.Add("Centering Complete")
+        End If
+
+        'If returnedMsg = "Complete" Then
+        '    dt.Rows.Add(returnedMsg & DateTime.Now.ToShortTimeString())
+        '    dgv_FCC.DataSource = dt
+        'Else
+        '    dt.Rows.Add("Error Receiving Centering Pattern return message.  Check Centering Pattern status on server.")
+        '    dt.Rows.Add("Here's what was sent: {0}", returnedMsg)
+        'End If
+    End Sub
+
+    Private Sub btn_Abort_Click(sender As Object, e As EventArgs) Handles btn_Abort.Click
+        Dim sendThisMsg As String = "abort"
+        Dim returnedMsg As String = Send(sendThisMsg)
+
+        If returnedMsg = "Aborting..." Then
+            dt.Rows.Add(returnedMsg & DateTime.Now.ToShortTimeString())
+            dgv_FCC.DataSource = dt
+        Else
+            dt.Rows.Add("Error Receiving Abort return message.  Check Abort status on server.")
+        End If
+    End Sub
+
+    Private Sub GroupBox2_Enter(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
